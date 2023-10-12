c910b2626def0785e3bcc49eabe0505b39659e9f
dipak-01 <imt_2022037@iiitm.ac.in>
2023-10-12 16:57:11 +0530

    Code for factorial with classes

---
+++ c++/Factorial.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Factorial
+{
+private:
+    int num;
+    unsigned long long factorial = 1;
+
+public:
+    void calculateFactorial();
+    void show();
+};
+
+void Factorial::calculateFactorial()
+{
+    cout << "Enter a number:" << endl;
+    cin >> num;
+
+    if (num == 0 || num == 1)
+    {
+        factorial = 1;
+    }
+    else
+    {
+        while (num > 1)
+        {
+            factorial = factorial * num;
+            num--;
+        }
+    }
+}
+
+void Factorial::show()
+{
+    cout << "Factorial: " << factorial << endl;
+}
+
+int main()
+{
+    Factorial factorial;
+    factorial.calculateFactorial();
+    factorial.show();
+}
