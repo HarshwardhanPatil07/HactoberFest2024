2f6c7b1551f9846b9cbe9800f934567768431a09
Aman Agrawal <114796892+Aman-Agrawal-22@users.noreply.github.com>
2023-10-14 11:16:38 +0530

    Create QuickSort_inDescendingOrder.cpp
    Given an array of integers, sort them in descending order using Quick Sort

---
+++ c++/QuickSort_inDescendingOrder.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] >= pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pivot = partition(arr, low, high);
+        quickSort(arr, low, pivot - 1);
+        quickSort(arr, pivot + 1, high);
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int *scores = (int *)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &scores[i]);
+    }
+
+    quickSort(scores, 0, n - 1);
+    printArray(scores, n);
+
+    free(scores);
+    return 0;
+}
