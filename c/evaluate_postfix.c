d1f705a7e803516cd0e93a89d085c410ba8677f8
kashish-219 <kashishkhatri003@gmail.com>
2023-10-14 15:15:12 +0530

    evaluation of postfix updated again

--- c/evaluate_postfix.c
+++ c/evaluate_postfix.c
@@ -1,4 +1,4 @@
-//github user: kashish-219
+//github user: kashish-219 [Kashish Khatri] https://github.com/kashish-219
 //aim: to complete hactoberfest and learn github
 //date: 14/10/23
 #include <stdio.h>
