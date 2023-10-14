0877ee8de834885dd1cad3af1ab4f76330d0f757
kashish-219 <kashishkhatri003@gmail.com>
2023-10-14 15:10:25 +0530

    evaluation of postfix expression added

---
+++ c/evaluate_postfix.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <ctype.h>
+#define MAX 30
+
+float stack[MAX];
+int top=-1;
+
+void push(float stack[],float x);
+float pop(float stack[]);
+float evaluate(char exp[]);
+
+
+int main()
+{
+    char exp[MAX];
+    printf("\n Enter postfix expression: ");
+    scanf("%s",exp);
+    printf("\n The result is: ");
+    printf("%0.2f",evaluate(exp));
+}
+
+void push(float stack[],float x)
+{
+    if(top == MAX-1)
+    {
+        printf("\n OVERFLOW");
+    }
+    else
+    {
+        stack[++top] = x;
+    }
+}
+
+float pop(float stack[])
+{
+    if(top == -1)
+    {
+        return -1;
+    }
+    else
+    {
+        return stack[top--];
+    }
+}
+
+float evaluate(char exp[])
+{
+    int i=0;
+    float op1,op2,value;
+    while(exp[i] != '\0')
+    {
+        if(isdigit(exp[i]))
+        {
+            push(stack,(float)(exp[i]-'0'));
+        }
+        else
+        {
+            op2 = pop(stack);
+            op1 = pop(stack);
+            switch(exp[i])
+            {
+                case '+': value = op1+op2;
+                    break;
+                case '-': value = op1-op2;
+                    break;
+                case '*': value = op1*op2;
+                    break;
+                case '/': value = op1/op2;
+                    break;
+                case '%': value = (int)op1 % (int)op2;
+                    break;
+            }
+            push(stack,value);
+        }
+        i++;
+    }
+    return(pop(stack));
+}
