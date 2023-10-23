71dee69419a034b40a4bcca39f7cc9ab879895ed
Meet Ghodasara <75525703+meetghodasara@users.noreply.github.com>
2023-10-23 19:22:18 +0530

    Create BubbleSort.java

---
+++ SortingJava/BubbleSort.java
@@ -0,0 +1,45 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        
+        System.out.println("Original Array:");
+        printArray(arr);
+        
+        bubbleSort(arr);
+        
+        System.out.println("\nSorted Array:");
+        printArray(arr);
+    }
+    
+    // Bubble Sort function
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                // Compare adjacent elements and swap them if they are in the wrong order
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            
+            // If no two elements were swapped in the inner loop, the array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+    
+    // Utility function to print an array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
