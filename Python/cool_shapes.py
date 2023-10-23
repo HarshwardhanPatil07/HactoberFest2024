df9d8debbddc0d4f6e62e9a76d2bd962872631de
yuvrajverma25 <95195998+yuvrajverma25@users.noreply.github.com>
2023-10-23 15:40:54 +0530

    Add files via upload

---
+++ Python/cool_shapes.py
@@ -0,0 +1,18 @@
+import imp
+from turtle import*
+
+bgcolor('black')
+speed(0)
+hideturtle()
+
+circle(5*5.2)
+color('red')
+done()
+# for i in range(1):
+#     color('red')
+#     circle(i)
+#     color('orange')
+#     circle(i*0.0)
+#     right(3)
+#     forward(3)
+# done()
