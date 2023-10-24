20f28bc569c80cc8bdbf14fbd44aeb396082ddc4
Kunal Sharma <cse.190320027@silicon.ac.in>
2023-10-24 19:17:10 +0530

    Create Bubblesort.java

---
+++ SortingJava/Bubblesort.java
@@ -0,0 +1,29 @@
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11}; 
+        
+        int n = arr.length;
+        boolean a; 
+        
+        for (int i = 0; i < n - 1; i++) {
+            a = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    arr[j] = arr[j + 1] +arr[j];
+                    arr[j + 1] = arr[j]-arr[j+1];
+                    arr[j]=arr[j]-arr[j+1];
+                    a = true;
+                }
+            }
+            if (!a) {
+                break;
+            }
+        }
+        
+        System.out.print("Sorted array: ");
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+    }
+}
