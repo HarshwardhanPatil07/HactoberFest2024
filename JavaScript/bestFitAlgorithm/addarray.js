6d5be3d3de99d331b0172fbfb19745e02569c306
Sahil Shakoor <98870741+sahilshakoor786@users.noreply.github.com>
2023-10-26 03:15:09 +0530

    addarray.js
    Combining two arrays in javascript

---
+++ JavaScript/bestFitAlgorithm/addarray.js
@@ -0,0 +1,3 @@
+var arr1=['dad','adsa','adada','adada'] // arrray 1
+var arr2=['dad','adsawdawd','awdadadada','adadawad'] //array 2
+console.log(arr1.concat(arr2)) // get result of comibining array 1 with array 2
