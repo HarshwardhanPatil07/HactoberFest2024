13ce250e1ecc9c9427a5fc956951ce03b51fbd06
priyanshh007 <98622032+priyanshh007@users.noreply.github.com>
2023-10-25 15:47:22 +0530

    Create printingnumbers.cpp

---
+++ c++/printingnumbers.cpp
@@ -0,0 +1,12 @@
+// C++ Program to Print 1 to 100 Using For Loop
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i;
+    
+    for (i = 1; i <= 100; i++){
+        cout << i << " ";
+    }
+    return 0;
+}
