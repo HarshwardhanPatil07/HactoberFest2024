55114d74a11b25171915488c57809f6f62e26ac6
Garvit Juneja <144167902+garvit027@users.noreply.github.com>
2024-10-17 11:00:32 +0530

    please make hactoberfest accepted

---
+++ Python/bmi-calculator.py
@@ -0,0 +1,11 @@
+weight = int(input());
+height = float(input());
+x = weight/float(height*height);
+if x < 18.5:
+    print('Underweight')
+if x>=18.5 and x<25:
+    print("Normal")
+if x >= 25 and x < 30:
+   print('Overweight')
+if x >= 30:
+   print('Obesity')
