e39b09f2029240fd436db8bfe118a03e4cd6f450
Vijayalaxmi Wakode <vijayalaxmiwakode@gmail.com>
2024-10-19 01:59:26 +0530

    Update pyramid_of_stars.c

--- c/Patterns/pyramid_of_stars.c
+++ c/Patterns/pyramid_of_stars.c
@@ -1,16 +1,23 @@
+//A program to draw pyramid for given number of rows
 /*
-Full pyramid of: 
+
+Console Output:
+Enter the number of rows: <user input>
+5
+**Pyramid of 5 rows**
         *
       * * *
     * * * * *
   * * * * * * *
 * * * * * * * * *
+
 */
 #include <stdio.h>
 int main() {
    int i, space, rows, k = 0;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
+   printf("**Pyramid of 5 rows**\n");
    for (i = 1; i <= rows; ++i, k = 0) {
       for (space = 1; space <= rows - i; ++space) {
          printf("  ");
