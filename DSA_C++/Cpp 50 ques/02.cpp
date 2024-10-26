8a450ad76dc67c1726b955bbc9ad755a1a0d6ba8
devank21 <132289193+devank21@users.noreply.github.com>
2024-10-27 00:36:03 +0530

    Update 02.cpp
    hactoberfest 2024

--- DSA_C++/Cpp 50 ques/02.cpp
+++ DSA_C++/Cpp 50 ques/02.cpp
@@ -6,16 +6,21 @@ class num{
     int n;
 
     void display(int n){
-        cout<<"entered number"<<n<<endl;
+        cout<<"entered number : "<<n<<endl;//this is a function within which we just print out the number that han been entered by the user 
     }
 };
 
 int main(){
     int n;
-    cout<<"enter the number u want to display : ";
+    int m=1;
+    while(m==1){
+    cout<<"enter the number you want to display : ";
     cin>>n;
     num obj;
     obj.display(n);
+    cout<<"if you want to exit entering the numbers enter 0 or if you like to continue enter 1 " <<endl;
+    cin>>m;
+    }
     
     return 0;
 }
