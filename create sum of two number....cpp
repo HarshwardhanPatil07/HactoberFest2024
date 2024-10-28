f8df5ffa5a56c96934d61fe5d280bc9d47b2d4f3
VaishnaviUbarhande <vaishnavi15ubarhande@gmail.com>
2024-10-28 19:43:19 +0530

    Create create sum of two number....cpp

---
+++ create sum of two number....cpp
@@ -0,0 +1,21 @@
+#include <iostream>  
+
+int main() {
+    int num1, num2, sum;
+
+    
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    
+    sum = num1 + num2;
+
+
+    std::cout << "The sum is: " << sum << std::endl;
+
+    return 0;
+}
