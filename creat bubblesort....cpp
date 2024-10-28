5cff8d3b537056e55ffd4d5b06cb10299765b09b
VaishnaviUbarhande <vaishnavi15ubarhande@gmail.com>
2024-10-28 20:06:13 +0530

    Create creat bubblesort....cpp

---
+++ creat bubblesort....cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    cout << "Unsorted array: ";
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+    
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
